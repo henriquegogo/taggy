0fa7bab612ff120cb56e9433c0b1596756fd0c74
Henrique Gogó <henriquegogo@gmail.com>
2024-09-08 23:16:59 -0300

    Only apply a data-event attribute if it has an event listener

--- taggy.js
+++ taggy.js
@@ -1,26 +1,26 @@
 export default function(strs, ...vals) {
   const wrapper = document.createElement("div");
 
-  // Set attribute identifiers and slots to be replaced by instance references.
+  // Set slots to be replaced by instance references.
   wrapper.innerHTML = strs.map((str, i) => str + (
-    vals[i]?.constructor === Object ? `data-t="${i}"` 
+    vals[i]?.constructor === Object ? `data-i="${i}"`
     : Array.isArray(vals[i]) || vals[i] instanceof Node
-    ? `<slot data-t="${i}"></slot>` : vals[i] ?? "")).join("");
+    ? `<slot data-i="${i}"></slot>` : vals[i] ?? "")).join("");
 
-  // Replace attributes and slots with references to objects and elements.
-  wrapper.querySelectorAll("[data-t]").forEach(el =>
-    Array.isArray(vals[el.dataset.t]) ? el.replaceWith(...vals[el.dataset.t]) 
-    : vals[el.dataset.t] instanceof Node ? el.replaceWith(vals[el.dataset.t]) 
-    : Object.assign(el, vals[el.dataset.t], delete el.dataset.t));
+  // Replace slots with references to objects and elements.
+  wrapper.querySelectorAll("[data-i]").forEach(el =>
+    Array.isArray(vals[el.dataset.i]) ? el.replaceWith(...vals[el.dataset.i])
+    : vals[el.dataset.i] instanceof Node ? el.replaceWith(vals[el.dataset.i])
+    : Object.assign(el, vals[el.dataset.i], delete el.dataset.i));
 
   const el = wrapper.children[0] || wrapper;
 
   // Create essential functions to handle events and state.
-  el.dataset.event = "";
   el.self = el;
   el.update = (newEl) => el.replaceWith(newEl);
-  el.listen = (ev, fn) => el.addEventListener(ev, ({detail}) => fn(detail));
-  el.dispatch = (ev, detail) => document.querySelectorAll('[data-event]')
+  el.listen = (ev, fn) => (el.dataset.event = "", el)
+    .addEventListener(ev, ({detail}) => fn(detail));
+  el.dispatch = (ev, detail) => document.querySelectorAll("[data-event]")
     .forEach(el => el.dispatchEvent(new CustomEvent(ev, {detail})));
 
   return el;
